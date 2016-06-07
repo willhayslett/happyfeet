f124b030ec6d36b191d80b76fe653fb369ebcdb8
willhayslett <william.hayslettjr@gmail.com>
2016-06-06 20:46:27 -0500

    updated

--- sendComment.php
+++ sendComment.php
@@ -40,7 +40,7 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "<h1><strong>Success!</strong> We've got your comment and look forward to connecting with you soon.</h1>";
+            echo "<h3><strong>Success!</strong> We've got your comment and look forward to connecting with you soon.</h3>";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
